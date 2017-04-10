825c22e4214a9a142e168e7fba1db4ffbf5b7786
Victor Popescu <nomevictor@gmail.com>
2017-04-10 20:44:30 +0300

    Updated version
    Updated version of preloading backgrounds, Updated stylesheet

--- Intermediate Front End Development Projects/Random Quote Machine/script.js
+++ Intermediate Front End Development Projects/Random Quote Machine/script.js
@@ -1,112 +1,147 @@
+var preloaded_images = [];
+var imageCount = 0;
+
+var images = ["https://source.unsplash.com/category/nature/1920x1080",
+    "https://source.unsplash.com/category/nature/1920x1081",
+    "https://source.unsplash.com/category/nature/1920x1082",
+    "https://source.unsplash.com/category/nature/1921x1080",
+    "https://source.unsplash.com/category/nature/1921x1081",
+    "https://source.unsplash.com/category/nature/1921x1082"
+];
+
+
+/* images is an array with image metadata */
+preload(images);
+
+
 /**
- * @module RandomQuoteComponent
+ * 
+ * @param {number} brightness 
+ */
+var set_brightness = function (brightness) {
+
+    if (brightness < 100) {
+        $('#randomQuote').css({
+            'background-color': 'rgba(0,0,0,0.4)',
+            'filter': 'alpha(opacity=40)'
+        });
+        $('#content').css({
+            'color': 'white',
+        });
+        $('#source').css({
+            'color': 'white'
+        });
+        $('#getQuote').css({
+            'background-color': 'rgba(255,255,255,0.4)',
+            'color': 'black'
+        });
+    } else {
+        $('#randomQuote').css({
+            'background-color': 'rgba(255,255,255,0.4)',
+            'filter': 'alpha(opacity=40)'
+        });
+        $('#content').css({
+            'color': 'black',
+        });
+        $('#source').css({
+            'color': 'black'
+        });
+        $('#getQuote').css({
+            'background-color': 'rgba(0,0,0,0.4)',
+            'color': 'white'
+        });
+    }
+}
+
+
+
+/**
+ * 
+ * @param {image} img 
+ * @param {function} changeBrightness 
+ */
+function get_bg_brightness(img, changeBrightness) {
+
+    var colorSum = 0;
+
+    // create canvas
+    var canvas = document.createElement("canvas");
+    canvas.width = this.width;
+    canvas.height = this.height;
+
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(this, 0, 0);
+
+    var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    var data = imageData.data;
+    var r, g, b, avg;
+
+    for (var x = 0, len = data.length; x < len; x += 4) {
+        r = data[x];
+        g = data[x + 1];
+        b = data[x + 2];
+
+        avg = Math.floor((r + g + b) / 3);
+        colorSum += avg;
+    }
+
+    var brightness = Math.floor(colorSum / (this.width * this.height));
+
+    changeBrightness(brightness);
+
+}
+
+
+function preload(images, index) {
+
+    index = index || 0;
+
+    if (images && images.length > index) {
+
+        var img = new Image();
+        img.src = images[index];
+
+        img.onload = function () {
+
+            preloaded_images.push(img);
+            $('#main').css("background-image", "url(" + preloaded_images[index].src + ")");
+            
+
+            //get_bg_brightness(img, set_brightness);
+            setTimeout(function () {
+
+                preload(images, index + 1);
+
+            }, 10000);
+        }
+    } else {
+
+        index = 0;
+        preloaded_images = [];
+        preload(images, index);
+    }
+}
+
+
+/**
  * @author Victor Popescu {@link https://github.com/VicPopescu}
  * @description Component used to fetch a random quote from an API provided by {@link https://quotesondesign.com/api-v4-0/ QuotesOnDesign}
  */
-function RandomQuoteComponent() {
+var RandomQuoteComponent = function () {
 
-    //vars
+    //selectors
     var root = $('main'),
         quote_container = $('#randomQuote'),
         quote_title = $('#title'),
         quote_content = $('#quote'),
         quote_source = $('#source');
 
-    var api_bg_image_link = "https://source.unsplash.com/category/nature/1920x1080#" + new Date().getTime();
-    var api_quote_link = "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=?";
+    var random_quote = Api.get_quote();
+    //var random_bgImg = Api.get_bg();
 
-
-    get_bg_brightness(api_bg_image_link, function (brightness) {
-
-        if (brightness < 100) {
-            $('#randomQuote').css({
-                'background-color': 'rgba(0,0,0,0.4)',
-                'filter': 'alpha(opacity=40)'
-            });
-            $('#content').css({
-                'color': 'white',
-            });
-            $('#source').css({
-                'color': 'white'
-            });
-            $('#getQuote').css({
-                'background-color': 'rgba(255,255,255,0.4)',
-                'color': 'black'
-            });
-        } else {
-            $('#randomQuote').css({
-                'background-color': 'rgba(255,255,255,0.4)',
-                'filter': 'alpha(opacity=40)'
-            });
-            $('#content').css({
-                'color': 'black',
-            });
-            $('#source').css({
-                'color': 'black'
-            });
-            $('#getQuote').css({
-                'background-color': 'rgba(0,0,0,0.4)',
-                'color': 'white'
-            });
-        }
+    random_quote.done(function (data) {
+        display_quote(data[0]);
     });
-
-    //API: get random quote
-    var quote_API = $.ajax({
-
-        url: api_quote_link,
-        dataType: 'jsonp', //using jsonp to allow CORS (Cross-origin resource sharing)
-        success: function (data) {
-
-            //display quote on page
-            display_quote(data[0]);
-        },
-        error: function () {
-            alert("Something went wrong with the server...");
-        }
-    });
-
-
-    function get_bg_brightness(imageSrc, callback) {
-
-        var img = document.createElement("img");
-        img.crossOrigin = "Anonymous";
-        img.src = imageSrc;
-        //img.style.display = "none";
-        //document.body.appendChild(img);
-        var colorSum = 0;
-
-        img.onload = function () {
-
-        $('body').css('background-image', "url(" + img.src + ")");
-            
-
-
-            // create canvas
-            var canvas = document.createElement("canvas");
-            canvas.width = this.width;
-            canvas.height = this.height;
-
-            var ctx = canvas.getContext("2d");
-            ctx.drawImage(this, 0, 0);
-
-            var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-            var data = imageData.data;
-            var r, g, b, avg;
-
-            for (var x = 0, len = data.length; x < len; x += 4) {
-                r = data[x];
-                g = data[x + 1];
-                b = data[x + 2];
-
-                avg = Math.floor((r + g + b) / 3);
-                colorSum += avg;
-            }
-
-            var brightness = Math.floor(colorSum / (this.width * this.height));
-            callback(brightness);
-        }
-    }
 
     /**
      * @private
@@ -124,14 +159,47 @@ function RandomQuoteComponent() {
 
 
         quote_container.data("quote-id", id);
-        //quote_container.fadeOut(0, function () {
-        //$(this).data("quote-id", id).fadeIn(1000);
         quote_content.html(content);
         quote_source.text(title).prop('href', source);
-        //});
-
     };
 };
 
 
+/**
+ * @description API providers
+ */
+var Api = (function () {
+
+
+    /**
+     * @description Get a random quote from {@link http://quotesondesign.com/}
+     */
+    var get_quote = function () {
+
+        return $.ajax({
+            url: "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=?",
+            dataType: 'jsonp', //using jsonp to allow CORS (Cross-origin resource sharing)
+            success: function (data) {
+                //display_quote(data[0]);
+            },
+            error: function () {
+                alert("Something went wrong with the server...");
+            }
+        });
+    };
+
+
+    /**
+     * Public exports
+     */
+    var PUBLIC = {
+
+        get_quote: get_quote
+    }
+
+    return PUBLIC;
+})();
+
+
 $('#getQuote').on("click.getNewQuote", RandomQuoteComponent);
+$('#getQuote').trigger('click.getNewQuote');
