46a88fa5ba5b6d385589be5b9ec1d446c8b26c0c
Victor Popescu <nomevictor@gmail.com>
2017-05-08 18:43:13 +0300

    Major Update
    +display temperature and precipitations informations
    +unit measurement support for different zones
    +charts color gradients
    +chart tooltips will display more details
    +others I forgot about...

--- Intermediate Front End Development Projects/Show the Local Weather/script.js
+++ Intermediate Front End Development Projects/Show the Local Weather/script.js
@@ -5,6 +5,8 @@
 
 //Global DOM selectors
 var $root = $('main');
+var $chartContainer = $('#chartContainer');
+var $weatherChart = $('#weatherChart');
 
 /**
  * @public
@@ -82,6 +84,32 @@ var Helpers = (function () {
 
 
     /**
+     * @private
+     * @description Parse timestamp and returns only the hour
+     */
+    var get_hourFromTimestamp = function (time) {
+
+        var t = new Date(time * 1000).toLocaleString();
+
+        return t.replace(/.+\,/, '');
+    };
+
+
+    /**
+     * @private
+     * @description Format number to display only 2 digits. eg. 32.00023001 = 32.00
+     */
+    var get_twoDecimalNumber = function (nr) {
+
+        if (!Number.isInteger(nr)) {
+            console.log(nr);
+            return Number.parseFloat(Math.round(nr * 100) / 100).toFixed(2);
+        } else {
+            return nr;
+        }
+    };
+
+    /**
      * Public Exports
      */
     var PUBLIC = {
@@ -89,7 +117,10 @@ var Helpers = (function () {
         set_locationDetails: set_locationDetails,
         //gets
         get_location_coords: get_location_coords,
-        get_locationDetails: get_locationDetails
+        get_locationDetails: get_locationDetails,
+        get_hourFromTimestamp: get_hourFromTimestamp,
+        get_twoDecimalNumber: get_twoDecimalNumber
+
     };
 
     return PUBLIC;
@@ -99,17 +130,14 @@ var Helpers = (function () {
 
 /**
  * @public
- * @description 
+ * @description Weather informations handling
  */
 var Weather = (function () {
 
-    //var Currently;        //Current weather contitions at the requested location
-    //var Daily;            //Weather conditions hour-by-hour for the next two days
-    //var Hourly;           //Weather conditions day-by-day for the next week.
-    //var Alerts;           //If present, contains any severe weather alerts pertinent to the requested location.
 
     //weather details
     var weather_details = {};
+    var units;
 
 
     /**
@@ -119,11 +147,7 @@ var Weather = (function () {
     var set_weatherDetails = function (weatherData) {
 
         weather_details = weatherData;
-
-        // Currently = weatherData.currently || null;
-        // Daily = weatherData.daily || null;
-        // Hourly = weatherData.daily || null;
-        // Alerts = weatherData.alerts || null;
+        units = weather_details.flags.units;
     };
 
 
@@ -136,17 +160,269 @@ var Weather = (function () {
         return weather_details;
     };
 
+
+    /**
+     * @private
+     * @description Set unit measurement for different zones (eg. EU: Degrees Celsius for Temperature, Millimeters per hour for Precipitations etc)
+     */
+    var get_weatherUnits = function () { //TODO: add units for all tracked weather information
+
+        var localUnits = {};
+        //todo add units for more (wind, etc)
+        switch (units) {
+            case 'ca':
+                localUnits = {
+                    t: '\u00B0C',
+                    //p: 'millimeters/h'        example for precipitations
+                };
+                break;
+            case 'si':
+                localUnits = {
+                    t: '\u00B0C'
+                };
+                break;
+            case 'uk2':
+                localUnits = {
+                    t: '\u00B0C'
+                };
+                break;
+            case 'us':
+                localUnits = {
+                    t: '\u00B0F'
+                };
+                break;
+            default:
+                break;
+        }
+        return localUnits;
+    };
+
     /**
      * Public Exports
      */
     var PUBLIC = {
 
         set_weatherDetails: set_weatherDetails,
-        get_weatherDetails: get_weatherDetails
+        get_weatherDetails: get_weatherDetails,
+        get_weatherUnits: get_weatherUnits
     };
 
     return PUBLIC;
 })();
+
+
+
+/**
+ * @public
+ * @description Charts handling. Provider: Chart.js/2.5.0
+ */
+var Charts = (function () {
+
+    var create_chart = function ($container, providedData, customType, customOptions) {
+
+        //chart container
+        var ctx = $container;
+
+        var temperatureData = [];
+        var tempDetails = [];
+        var precipDetails = [];
+        var precipitationsData = [];
+        var weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        var days = [];
+
+
+
+        providedData.data.forEach(function (weatherPerDay, index) {
+
+            temperatureData.push(weatherPerDay.temperatureMax);
+            precipitationsData.push((weatherPerDay.precipProbability * 100).toFixed());
+
+            var time = new Date(weatherPerDay.time * 1000);
+            var dayName = weekDays[time.getDay()];
+            days.push(dayName);
+
+
+            tempDetails.push([
+                'Max: ' + weatherPerDay.temperatureMax + Weather.get_weatherUnits().t,
+                'MaxTime: ' + Helpers.get_hourFromTimestamp(weatherPerDay.temperatureMaxTime),
+                'Min: ' + weatherPerDay.temperatureMin + Weather.get_weatherUnits().t,
+                'MinTime: ' + Helpers.get_hourFromTimestamp(weatherPerDay.temperatureMinTime),
+            ]);
+
+            precipDetails.push([
+                'Probability: ' + (weatherPerDay.precipProbability * 100).toFixed() + '%',
+                'Intensity: ' + (weatherPerDay.precipIntensity * 100).toFixed() + '%',
+                'IntensityMax: ' + (weatherPerDay.precipIntensityMax * 100).toFixed() + '%',
+                'Type: ' + weatherPerDay.precipType
+            ]);
+        });
+
+        var c = document.getElementById("weatherChart");
+        var d = c.getContext("2d");
+
+        var temperatureGradient = d.createLinearGradient(0, 0, 0, 450);
+        temperatureGradient.addColorStop(0, 'rgba(255, 102, 0, 0.8)');
+        temperatureGradient.addColorStop(0.5, 'rgba(255, 102, 0, 0.25)');
+        temperatureGradient.addColorStop(1, 'rgba(255, 102, 0, 0)');
+
+        var precipitationsGradient = d.createLinearGradient(0, 0, 0, 450);
+        precipitationsGradient.addColorStop(0, 'rgba(0, 204, 204, 0.8)');
+        precipitationsGradient.addColorStop(0.5, 'rgba(0, 204, 204, 0.25)');
+        precipitationsGradient.addColorStop(1, 'rgba(0, 204, 204, 0)');
+
+
+        var temperature = {
+            label: "Temperature",
+            data: temperatureData,
+            moreDetails: tempDetails,
+            backgroundColor: temperatureGradient,
+            borderColor: "rgba(255, 51, 0, 1)",
+            borderCapStyle: 'butt',
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            borderWidth: 1,
+            pointBorderColor: "rgba(255, 51, 0, 1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(255, 51, 0, 1)",
+            pointHoverBorderColor: "rgba(255, 51, 0, 1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            yAxisID: 'Temp'
+        };
+
+        var precipitations = {
+            label: "Precipitations",
+            data: precipitationsData,
+            moreDetails: precipDetails,
+            backgroundColor: precipitationsGradient,
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: 'butt',
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            borderWidth: 1,
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            yAxisID: 'Precip'
+        };
+
+
+        //build chart
+        var myChart = new Chart(ctx, {
+            type: customType || 'bar',
+            data: {
+                labels: days,
+                datasets: [precipitations, temperature]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: true,
+                animation: {
+                    easing: 'easeInOutQuad',
+                    duration: 520
+                },
+                scales: {
+                    xAxes: [{
+                        gridLines: {
+                            color: 'rgba(200, 200, 200, 0.05)'
+                        }
+                    }],
+                    yAxes: [{
+                        id: 'Temp',
+                        type: 'linear',
+                        position: 'left',
+                        ticks: {
+                            max: 160,
+                            min: 0,
+                            fontColor: temperatureGradient,
+                            callback: function (label, index, labels) {
+                                return label + Weather.get_weatherUnits().t;
+                            }
+                        },
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Temperature ' + Weather.get_weatherUnits().t
+                        }
+                    }, {
+                        id: 'Precip',
+                        type: 'linear',
+                        position: 'right',
+                        ticks: {
+                            max: 100,
+                            min: 0,
+                            fontColor: precipitationsGradient,
+                            callback: function (label, index, labels) {
+                                return label + '%';
+                            }
+                        },
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Precipitations'
+                        },
+                        gridLines: {
+                            color: 'rgba(200, 200, 200, 0.08)'
+                        }
+                    }]
+                },
+                elements: {
+                    line: {
+                        tension: 0.4
+                    }
+                },
+                legend: {
+                    position: 'bottom'
+                },
+                point: {
+                    backgroundColor: 'green'
+                },
+                tooltips: {
+                    titleFontFamily: 'Open Sans',
+                    titleSpacing: 10,
+                    titleFontColor: 'white',
+                    backgroundColor: 'rgba(49, 49, 52, 0.7)',
+                    caretSize: 5,
+                    bodySpacing: 8,
+                    cornerRadius: 2,
+                    xPadding: 10,
+                    yPadding: 10,
+                    callbacks: {
+                        label: function (tooltipItems, data) {
+
+                            var datasetIndex = tooltipItems.datasetIndex;
+                            var tooltopIndex = tooltipItems.index;
+
+                            return data.datasets[datasetIndex].moreDetails[tooltopIndex];
+                        }
+                    }
+                }
+            }
+        });
+
+
+    };
+
+
+
+    /**
+     * Public Exports
+     */
+    var PUBLIC = {
+
+        create_chart: create_chart
+    };
+
+    return PUBLIC;
+})();
+
 
 /**
  * @public
@@ -160,7 +436,7 @@ var Api = (function () {
      */
     var get_weatherInfo = function (latitude, longitude) {
 
-        var url = "https://crossorigin.me/https://api.darksky.net/forecast/d212e752e77024fa82c5713e0debad8b/" + latitude + "," + longitude + "?exclude=flags,minutely";
+        var url = "https://crossorigin.me/https://api.darksky.net/forecast/d212e752e77024fa82c5713e0debad8b/" + latitude + "," + longitude + "?units=auto&exclude=minutely";
 
         return $.ajax({
             type: 'GET',
@@ -204,55 +480,14 @@ var WeatherApp = (function () {
         var weatherInfo = Api.get_weatherInfo(Helpers.get_locationDetails().lat, Helpers.get_locationDetails().long);
         //then:
         weatherInfo.done(function (statistics) {
-
+            console.log(statistics);
             //construct location details
             //Helpers.set_locationDetails(statistics);
             //construct weather details
             Weather.set_weatherDetails(statistics);
-            //
 
-            console.log(Weather.get_weatherDetails());
-
-            var ctx = $("#weatherChart");
-
-            var myChart = new Chart(ctx, {
-                type: 'line',
-                data: {
-                    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-                    datasets: [{
-                        label: '# of Votes',
-                        data: [12, 19, 3, 5, 2, 7],
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgba(255,99,132,1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
-                        ],
-                        borderWidth: 1
-                    }]
-                },
-                options: {
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero: true
-                            }
-                        }]
-                    }
-                }
-            });
-
-            
+            //create chart ($container, customChartData, customType, customOptions)
+            Charts.create_chart($weatherChart, Weather.get_weatherDetails().daily, 'line', null);
         });
     });
 })();
