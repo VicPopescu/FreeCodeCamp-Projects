401a3978263d5d23dca8053a4591822e7cce32fb
Victor Popescu <nomevictor@gmail.com>
2017-07-31 09:53:22 +0300

    Javascript calculator project setup

---
+++ Advanced Front End Development Projects/JavaScript Calculator/script.js
@@ -0,0 +1,31 @@
+/**
+ * @description A javascript calculator
+ * @author Victor Popescu {@link https://github.com/VicPopescu}
+ */
+var CALCULATOR = (function () {
+
+    //Global DOM selectors
+    var $document = $('html');
+    var $root = $('#main');
+
+    /**
+     * 
+     */
+    var Helpers = (function () {
+
+        var PUBLIC = {
+
+        };
+
+        return PUBLIC;
+    })();
+
+    /**
+     * 
+     */
+    var PotatoCalculator = (function () {
+
+        //
+    })();
+
+})();
